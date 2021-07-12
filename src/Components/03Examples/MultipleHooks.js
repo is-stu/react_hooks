2ab708b5a6ce9d500eb3c9d43265d57d67a1b815
Stewar Marin <stewar1020@gmail.com>
2021-07-12 08:50:20 -0500

    Adding next quote button

--- src/Components/03Examples/MultipleHooks.js
+++ src/Components/03Examples/MultipleHooks.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { useCounter } from '../../Hooks/useCounter';
 import { useFetch } from '../../Hooks/useFetch';
 import './MultipleHooks.css';
 
 export const MultipleHooks = () => {
-  const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-  console.log(state);
+  const { counter, increment } = useCounter(1);
+  const { data, loading } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
+  const { author, quote } = !!data && data[0];
 
   return (
     <div>
-      <h1>Custom Hooks</h1>
+      <h1>Breaking Bad Quotes</h1>
       <hr />
+
+      <hr />
+
+      {loading ? (
+        <div className='alert alert-info text-center'>Loading...</div>
+      ) : (
+        <blockquote className='blockquote text-right'>
+          <p className='mb-3'> {quote} </p>
+          <footer className='blockquote-footer'>{author}</footer>
+          <button className='btn btn-outline-primary' onClick={increment}>
+            Next Quote
+          </button>
+        </blockquote>
+      )}
     </div>
   );
 };
