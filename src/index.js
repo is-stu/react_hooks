7fbe2fe4e73a5e16ee9ec2c5ccb51c351f7cdca1
Stewar Marin <stewar1020@gmail.com>
2021-07-19 11:42:16 -0500

    Starting useReducer component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './Components/07-tarea-memo/Padre';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './Components/07-tarea-memo/Padre';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Padre />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import './Components/08-useReducer/intro-reducer';
