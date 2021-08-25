df5ed94b01f620c3c6e42f46d522bfe0fd447a11
Stewar Marin <stewar1020@gmail.com>
2021-08-25 11:30:59 -0500

    Adding first example with useContext

--- src/Components/09-useContext/HomeScreen.js
+++ src/Components/09-useContext/HomeScreen.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UseContext';
 
 export const HomeScreen = () => {
+  const userContext = useContext(UserContext);
+  console.log(userContext);
   return (
     <div>
       <h1>HomeScreen App here</h1>
--- src/Components/09-useContext/MainApp.js
+++ src/Components/09-useContext/MainApp.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UseContext';
 
 export const MainApp = () => {
-  return <AppRouter />;
+  const user = {
+    id: 123,
+    name: 'Stewar',
+    email: 'stewar1020@gmail.com',
+  };
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
